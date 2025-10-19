419a315a92c0ada4cfbe2d8894051c4b57dbb177
constructoracsrsas <constructoracsrsas@gmail.com>
2025-10-18 21:04:00 -0700

    Remove tooltip dependency from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Dashboard from "./pages/Dashboard";
@@ -11,7 +10,7 @@ import ControlCalidad from "./pages/ControlCalidad";
 import NotFound from "./pages/NotFound";
 
 const App = () => (
-  <TooltipProvider>
+  <>
     <Toaster />
     <BrowserRouter>
       <Routes>
@@ -25,7 +24,7 @@ const App = () => (
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  </TooltipProvider>
+  </>
 );
 
 export default App;
