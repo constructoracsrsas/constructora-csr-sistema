0763d70e34385aef8ff00749e8af9008c931afd7
constructoracsrsas <constructoracsrsas@gmail.com>
2025-10-19 21:08:33 -0700

    Fix card.tsx formatting

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -75,4 +75,5 @@ const CardFooter = React.forwardRef<
 ))
 CardFooter.displayName = "CardFooter"
 
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent "}"
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+  "}"
